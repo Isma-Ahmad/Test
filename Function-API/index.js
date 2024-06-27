4490b4662e07048951547e25050535441ae4f0d8
Isma-Ahmad <ismaa7990@gmail.com>
2024-06-27 15:18:21 +0500

    Delete Function-API directory
    delete files

--- Function-API/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-const PORT = 8006;
-const userRoute = require("./route/userAPI");
-
-app.use("/user", userRoute );
-
-app.listen(PORT, ()=>{ console.log("Sever is start")});
